a642a03a2c41a196d6ddd25728ba770fa8c0b1dc
ldldz <qkrqudrjs4350@gmail.com>
2024-05-17 10:56:33 +0900

    Feat: fetchJob 함수에 검색 기능 추가

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,16 +1,30 @@
 import { Job } from "./definitions";
 import clientPromise from "./mongodb";
 
-export default async function fetchJobs() {
+export default async function fetchJobs(searchValue?: string) {
   try {
     const client = await clientPromise;
+    const coll = client.db("jobBoard").collection<Job>("jobs");
 
-    const jobs: Job[] = await client
-      .db("jobBoard")
-      .collection<Job>("jobs")
-      .find()
-      .toArray();
-    return jobs;
+    if (searchValue) {
+      return coll
+        .aggregate<Job>([
+          {
+            $search: {
+              index: "job_title_index",
+              text: {
+                query: searchValue,
+                path: {
+                  wildcard: "*",
+                },
+              },
+            },
+          },
+        ])
+        .toArray();
+    }
+
+    return coll.find().toArray();
   } catch (e) {
     console.error(e);
   }
