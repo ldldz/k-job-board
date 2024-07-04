02108db5134a5b1c1697307d524565a0fa43dfb4
ldldz <qkrqudrjs4350@gmail.com>
2024-07-04 16:53:00 +0900

    feat: Add pagination feature and UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 import JobCardsList from "@/components/home/JobCardsList";
 import Main from "@/components/home/Main";
 import SearchBar from "@/components/home/SearchBar";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 import { Separator } from "@/components/ui/separator";
 
 export default function Home({
@@ -11,8 +19,21 @@ export default function Home({
     page?: string;
   };
 }) {
+  const PAGINATION_LENGTH = 5;
   const query = searchParams?.query || "";
-  const currentPage = Number(searchParams?.page); // page가 없으면 0
+  const currentPage = Number(searchParams?.page || 1); // page가 없으면 1
+  const paginationNumbers = Array.from(
+    { length: PAGINATION_LENGTH },
+    (_, i) =>
+      Math.floor((currentPage - 1) / PAGINATION_LENGTH) * PAGINATION_LENGTH +
+      i +
+      1,
+  );
+  const prevPage =
+    paginationNumbers[0] - PAGINATION_LENGTH > 0
+      ? paginationNumbers[0] - PAGINATION_LENGTH
+      : 1;
+  const nextPage = currentPage + PAGINATION_LENGTH;
 
   return (
     <div className="flex flex-col items-center">
@@ -22,6 +43,33 @@ export default function Home({
       </div>
       <Separator />
       <JobCardsList query={query} currentPage={currentPage} />
+      <div className="my-4">
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious
+                href={{ query: { ...searchParams, page: prevPage } }}
+              />
+            </PaginationItem>
+            {paginationNumbers.map((paginationNumber) => (
+              <PaginationItem key={paginationNumber}>
+                <PaginationLink
+                  href={{ query: { ...searchParams, page: paginationNumber } }}
+                  isActive={currentPage === paginationNumber}
+                >
+                  {paginationNumber}
+                </PaginationLink>
+              </PaginationItem>
+            ))}
+
+            <PaginationItem>
+              <PaginationNext
+                href={{ query: { ...searchParams, page: nextPage } }}
+              />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
     </div>
   );
 }
---
+++ src/components/home/JobsPagination.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+
+export default function JobsPagination() {
+  return (
+    <div className="my-4">
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href={``} />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </div>
+  );
+}
