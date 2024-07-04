a676c2764ccc76d4bfc211c5beb67a6c8062f040
ldldz <qkrqudrjs4350@gmail.com>
2024-07-04 19:28:31 +0900

    feat: Add web analytics by vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const pretendard = localFont({
   src: "../static/fonts/PretendardVariable.woff2",
@@ -21,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html className={`${pretendard.variable}`}>
-      <body className={pretendard.className}>{children}</body>
+      <body className={pretendard.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
