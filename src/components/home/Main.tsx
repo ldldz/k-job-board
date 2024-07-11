fe32b9628f7a9c5ae9661e2110387e60bb483ad5
ldldz <qkrqudrjs4350@gmail.com>
2024-07-11 21:44:00 +0900

    feat: Add homepage navigation on logo click

--- src/components/home/Main.tsx
+++ src/components/home/Main.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import SubscribeModal from "./SubscribeDialog";
 
 export default function Main() {
   return (
     <main className="my-4 flex w-full flex-col items-center	">
       <div className="flex flex-col items-center">
-        <h1 className="text-5xl font-extrabold">CareerLib🧑‍💻</h1>
+        <h1 className="text-5xl font-extrabold">
+          <Link href="/">CareerLib🧑‍💻</Link>
+        </h1>
         <div>스타트업의 채용공고를 한곳에서 만나보세요</div>
         <div className="mt-8">
           <SubscribeModal />
