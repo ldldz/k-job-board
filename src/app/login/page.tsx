1350f76cd0278beac57feabf43e1455906079df6
ldldz <qkrqudrjs4350@gmail.com>
2024-07-25 20:46:28 +0900

    feat: 공통 header 추가
    - layout에 공통 header 추가
    - 비로그인시 로그인 버튼 표시
    - 로그인시 프로필 이미지 표시, 클릭 시 마이페이지, 로그아웃 드롭다운 메뉴

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,36 +1,30 @@
 import { Button } from "@/components/ui/button";
-import { signIn, signOut } from "./actions";
+import { signIn } from "./actions";
 import { createClient } from "@/utils/supabase/server";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { redirect } from "next/navigation";
 
 export default async function LoginPage() {
   const supabase = createClient();
 
-  const { data, error } = await supabase.auth.getUser();
+  const { data } = await supabase.auth.getUser();
+
+  if (data.user) redirect("/");
+
   return (
     <div className="flex min-h-screen items-center justify-center">
       <Card className="w-96">
-        {!data.user ? (
-          <>
-            <CardHeader>
-              <CardTitle>로그인</CardTitle>
-              <CardDescription>Careerlib에 로그인하고 채용 정보를 받아보세요!</CardDescription>
-            </CardHeader>
-            <CardContent className="flex justify-center">
-              <form>
-                <Button className="bg-yellow-300 text-black" formAction={signIn}>
-                  카카오로 로그인
-                </Button>
-              </form>
-            </CardContent>
-          </>
-        ) : (
-          <CardContent className="flex justify-center p-6">
-            <form>
-              <Button formAction={signOut}>로그아웃</Button>
-            </form>
-          </CardContent>
-        )}
+        <CardHeader>
+          <CardTitle>로그인</CardTitle>
+          <CardDescription>Careerlib에 로그인하고 채용 정보를 받아보세요!</CardDescription>
+        </CardHeader>
+        <CardContent className="flex justify-center">
+          <form>
+            <Button className="bg-yellow-300 text-black" formAction={signIn}>
+              카카오로 로그인
+            </Button>
+          </form>
+        </CardContent>
       </Card>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import { JobBoard, Main, SearchBar } from "@/app/components";
-import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Suspense } from "react";
-import { createClient } from "@/utils/supabase/server";
-import Link from "next/link";
 
 export default async function Page({
   searchParams,
@@ -12,26 +9,9 @@ export default async function Page({
     [key: string]: string | string[] | undefined;
   };
 }) {
-  const supabase = createClient();
-
-  const { data } = await supabase.auth.getUser();
-
   return (
     <Suspense>
       <div className="flex flex-col items-center">
-        <div className="w-full">
-          <nav className="mx-auto flex max-w-8xl justify-end px-8 py-4">
-            {!data.user ? (
-              <Button asChild>
-                <Link href={"/login"}>로그인</Link>
-              </Button>
-            ) : (
-              <div>
-                <Link href={"/my"}>{data.user.user_metadata.name}</Link>
-              </div>
-            )}
-          </nav>
-        </div>
         <Main />
         <div className="flex w-full justify-center py-4">
           <SearchBar />
