1302bdcfd1c3189ccdc5f1342cee905b8beb0f0a
ldldz <qkrqudrjs4350@gmail.com>
2024-09-01 01:01:30 +0900

    style: code formatting

--- src/lib/supabase/types.ts
+++ src/lib/supabase/types.ts
@@ -1,186 +1,180 @@
-export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json | undefined }
-  | Json[]
+export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
 
 export type Database = {
   graphql_public: {
     Tables: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
       graphql: {
         Args: {
-          operationName?: string
-          query?: string
-          variables?: Json
-          extensions?: Json
-        }
-        Returns: Json
-      }
-    }
+          operationName?: string;
+          query?: string;
+          variables?: Json;
+          extensions?: Json;
+        };
+        Returns: Json;
+      };
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
   public: {
     Tables: {
       bookmarks: {
         Row: {
-          created_at: string
-          id: number
-          job_post_id: string
-          user_id: string
-        }
+          created_at: string;
+          id: number;
+          job_post_id: string;
+          user_id: string;
+        };
         Insert: {
-          created_at?: string
-          id?: number
-          job_post_id: string
-          user_id?: string
-        }
+          created_at?: string;
+          id?: number;
+          job_post_id: string;
+          user_id?: string;
+        };
         Update: {
-          created_at?: string
-          id?: number
-          job_post_id?: string
-          user_id?: string
-        }
+          created_at?: string;
+          id?: number;
+          job_post_id?: string;
+          user_id?: string;
+        };
         Relationships: [
           {
-            foreignKeyName: "bookmarks_job_post_id_fkey"
-            columns: ["job_post_id"]
-            isOneToOne: false
-            referencedRelation: "job_post_details"
-            referencedColumns: ["id"]
+            foreignKeyName: "bookmarks_job_post_id_fkey";
+            columns: ["job_post_id"];
+            isOneToOne: false;
+            referencedRelation: "job_post_details";
+            referencedColumns: ["id"];
           },
           {
-            foreignKeyName: "bookmarks_job_post_id_fkey"
-            columns: ["job_post_id"]
-            isOneToOne: false
-            referencedRelation: "job_posts"
-            referencedColumns: ["id"]
+            foreignKeyName: "bookmarks_job_post_id_fkey";
+            columns: ["job_post_id"];
+            isOneToOne: false;
+            referencedRelation: "job_posts";
+            referencedColumns: ["id"];
           },
           {
-            foreignKeyName: "bookmarks_user_id_fkey"
-            columns: ["user_id"]
-            isOneToOne: false
-            referencedRelation: "users"
-            referencedColumns: ["id"]
+            foreignKeyName: "bookmarks_user_id_fkey";
+            columns: ["user_id"];
+            isOneToOne: false;
+            referencedRelation: "users";
+            referencedColumns: ["id"];
           },
-        ]
-      }
+        ];
+      };
       companies: {
         Row: {
-          created_at: string | null
-          id: number
-          name: string
-          signing_bonus: number | null
-          url: string | null
-        }
+          created_at: string | null;
+          id: number;
+          name: string;
+          signing_bonus: number | null;
+          url: string | null;
+        };
         Insert: {
-          created_at?: string | null
-          id?: never
-          name: string
-          signing_bonus?: number | null
-          url?: string | null
-        }
+          created_at?: string | null;
+          id?: never;
+          name: string;
+          signing_bonus?: number | null;
+          url?: string | null;
+        };
         Update: {
-          created_at?: string | null
-          id?: never
-          name?: string
-          signing_bonus?: number | null
-          url?: string | null
-        }
-        Relationships: []
-      }
+          created_at?: string | null;
+          id?: never;
+          name?: string;
+          signing_bonus?: number | null;
+          url?: string | null;
+        };
+        Relationships: [];
+      };
       job_posts: {
         Row: {
-          category: string | null
-          company_id: number
-          created_at: string
-          due_date: string | null
-          id: string
-          is_expired: boolean
-          title: string
-          updated_at: string
-          url: string
-        }
+          category: string | null;
+          company_id: number;
+          created_at: string;
+          due_date: string | null;
+          id: string;
+          is_expired: boolean;
+          title: string;
+          updated_at: string;
+          url: string;
+        };
         Insert: {
-          category?: string | null
-          company_id: number
-          created_at?: string
-          due_date?: string | null
-          id: string
-          is_expired?: boolean
-          title: string
-          updated_at: string
-          url: string
-        }
+          category?: string | null;
+          company_id: number;
+          created_at?: string;
+          due_date?: string | null;
+          id: string;
+          is_expired?: boolean;
+          title: string;
+          updated_at: string;
+          url: string;
+        };
         Update: {
-          category?: string | null
-          company_id?: number
-          created_at?: string
-          due_date?: string | null
-          id?: string
-          is_expired?: boolean
-          title?: string
-          updated_at?: string
-          url?: string
-        }
+          category?: string | null;
+          company_id?: number;
+          created_at?: string;
+          due_date?: string | null;
+          id?: string;
+          is_expired?: boolean;
+          title?: string;
+          updated_at?: string;
+          url?: string;
+        };
         Relationships: [
           {
-            foreignKeyName: "fk_company"
-            columns: ["company_id"]
-            isOneToOne: false
-            referencedRelation: "companies"
-            referencedColumns: ["id"]
+            foreignKeyName: "fk_company";
+            columns: ["company_id"];
+            isOneToOne: false;
+            referencedRelation: "companies";
+            referencedColumns: ["id"];
           },
           {
-            foreignKeyName: "job_posts_company_id_fkey"
-            columns: ["company_id"]
-            isOneToOne: false
-            referencedRelation: "companies"
-            referencedColumns: ["id"]
+            foreignKeyName: "job_posts_company_id_fkey";
+            columns: ["company_id"];
+            isOneToOne: false;
+            referencedRelation: "companies";
+            referencedColumns: ["id"];
           },
-        ]
-      }
-    }
+        ];
+      };
+    };
     Views: {
       job_post_details: {
         Row: {
-          category: string | null
-          company_name: string | null
-          due_date: string | null
-          id: string | null
-          is_expired: boolean | null
-          signing_bonus: number | null
-          title: string | null
-          url: string | null
-        }
-        Relationships: []
-      }
-    }
+          category: string | null;
+          company_name: string | null;
+          due_date: string | null;
+          id: string | null;
+          is_expired: boolean | null;
+          signing_bonus: number | null;
+          title: string | null;
+          url: string | null;
+        };
+        Relationships: [];
+      };
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type PublicSchema = Database[Extract<keyof Database, "public">];
 
 export type Tables<
   PublicTableNameOrOptions extends
@@ -193,66 +187,58 @@ export type Tables<
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+      Row: infer R;
     }
     ? R
     : never
-  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-        PublicSchema["Views"])
-    ? (PublicSchema["Tables"] &
-        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+    ? (PublicSchema["Tables"] & PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R;
       }
       ? R
       : never
-    : never
+    : never;
 
 export type TablesInsert<
-  PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
-    | { schema: keyof Database },
+  PublicTableNameOrOptions extends keyof PublicSchema["Tables"] | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+      Insert: infer I;
     }
     ? I
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I
+        Insert: infer I;
       }
       ? I
       : never
-    : never
+    : never;
 
 export type TablesUpdate<
-  PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
-    | { schema: keyof Database },
+  PublicTableNameOrOptions extends keyof PublicSchema["Tables"] | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+      Update: infer U;
     }
     ? U
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U
+        Update: infer U;
       }
       ? U
       : never
-    : never
+    : never;
 
 export type Enums<
-  PublicEnumNameOrOptions extends
-    | keyof PublicSchema["Enums"]
-    | { schema: keyof Database },
+  PublicEnumNameOrOptions extends keyof PublicSchema["Enums"] | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
     : never = never,
@@ -260,4 +246,4 @@ export type Enums<
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
     ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never
+    : never;
